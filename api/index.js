4e5d1f843212dfc1ec5db04c772af9ae62c85716
yvan noubom <44666229+istyvnoub@users.noreply.github.com>
2021-11-14 22:47:51 +0100

    first commit

---
+++ api/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+import  mongoose from "mongoose";
+import multer from "multer";
+import path from "path";
+
+dotenv.config();
+app.use(express.json());
+
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify:true
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+
+app.listen("5000", () => {
+  console.log("Backend is running.");
+});
